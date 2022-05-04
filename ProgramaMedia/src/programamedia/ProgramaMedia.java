4ed9b4bfe23af8f0e976e88136901966cc798c6e
cleberpb <36317652+cleberpb@users.noreply.github.com>
2022-05-04 12:28:42 -0300

    Condição Simples

---
+++ ProgramaMedia/src/programamedia/ProgramaMedia.java
@@ -0,0 +1,24 @@
+
+package programamedia;
+
+import java.util.Scanner;// foi importado
+
+public class ProgramaMedia {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner teclado = new Scanner(System.in);// foi criado o objeto teclado que vai (System.in) monitorar a entrada do teclado.
+        
+        System.out.print("Primeira nota: ");
+        float nota1 = teclado.nextFloat();
+        System.out.print("Segunda nota: ");
+        float nota2 = teclado.nextFloat();
+        float media = (nota1 + nota2) / 2;
+        System.out.println("A sua média é: " + media);
+        
+        if (media > 6) {
+            System.out.println("Parabéns você passou");
+        }
+    }
+    
+}
