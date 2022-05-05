7f8f0d8c4dc8716030dd3eaacad218ec63e7060f
cleberpb <36317652+cleberpb@users.noreply.github.com>
2022-05-05 13:51:47 -0300

    Estrutura de repetição while

---
+++ Contador01/src/contador01/Contador01.java
@@ -0,0 +1,15 @@
+
+package contador01;
+
+public class Contador01 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int cc = 1; // cc - contador de cambalhotas
+        while (cc <= 4) {
+            System.out.println("Cambalhota " + cc);
+            cc++;
+        }
+    }
+    
+}
