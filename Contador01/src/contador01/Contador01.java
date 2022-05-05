5b61b5affbbfe257896df67d5b44f9d25d8076e7
cleberpb <36317652+cleberpb@users.noreply.github.com>
2022-05-05 13:57:22 -0300

    Mudando Fluxo de Laço - continue

--- Contador01/src/contador01/Contador01.java
+++ Contador01/src/contador01/Contador01.java
@@ -5,10 +5,13 @@ public class Contador01 {
 
     public static void main(String[] args) {
         // TODO code application logic here
-        int cc = 1; // cc - contador de cambalhotas
-        while (cc <= 4) {
-            System.out.println("Cambalhota " + cc);
+        int cc = 0; // cc - contador de cambalhotas
+        while (cc < 10) {
             cc++;
+            if (cc == 5 || cc == 7 || cc == 9) {
+                continue;
+            }
+            System.out.println("Cambalhota " + cc);
         }
     }
     
