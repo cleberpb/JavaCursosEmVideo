48b8d887c967c5bcb65979f5a4dcae308642b250
cleberpb <36317652+cleberpb@users.noreply.github.com>
2022-05-11 11:29:28 -0300

    Laço de repetição do while02

---
+++ NumerosDoWhile/src/numerosdowhile/NumerosDoWhile.java
@@ -0,0 +1,23 @@
+
+package numerosdowhile;
+
+import java.util.Scanner;
+
+public class NumerosDoWhile {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int numero, soma = 0;
+        String resposta;
+        Scanner teclado = new Scanner(System.in);
+        do {            
+            System.out.print("Digite um número: ");
+            numero = teclado.nextInt();
+            soma += numero; // += significa, soma = soma + numero; 
+            System.out.print("Quer continuar? [S/N]");
+            resposta = teclado.next();
+        } while (resposta.equals("S"));
+        System.out.println("A soma de todos os valores é " + soma);
+    }
+    
+}
