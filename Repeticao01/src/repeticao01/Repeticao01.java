36d77f661867ae1cff76b9bac5ad61e29bffff86
cleberpb <36317652+cleberpb@users.noreply.github.com>
2022-05-11 11:09:32 -0300

    Laço repetição do while

---
+++ Repeticao01/src/repeticao01/Repeticao01.java
@@ -0,0 +1,15 @@
+
+package repeticao01;
+
+public class Repeticao01 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int contadorCambalhota = 0;
+        do {
+            System.out.println(contadorCambalhota);
+            contadorCambalhota++;
+        } while (contadorCambalhota < 4);
+    }
+    
+}
