dd64296bfdffd18c850f48096d5d785b842c3847
cleberpb <36317652+cleberpb@users.noreply.github.com>
2022-05-11 13:48:01 -0300

    Laço de repetição For

---
+++ RepeticaoFor/src/repeticaofor/RepeticaoFor.java
@@ -0,0 +1,14 @@
+
+package repeticaofor;
+
+public class RepeticaoFor {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // cc siguinifica contandorCambalhota
+        for (int cc = 10; cc <= 100; cc += 10) {
+            System.out.println(cc);
+        }
+    }
+    
+}
